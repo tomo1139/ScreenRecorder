89b55ffae608191de4b1430bacab9370bcab99bb
tomo1139 <tomo1139@gmail.com>
2020-05-16 22:34:10 +0900

    no message

---
+++ ScreenRecorder/app/src/main/java/develop/beta1139/screenrecorder/MainActivity.kt
@@ -0,0 +1,12 @@
+package develop.beta1139.screenrecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
